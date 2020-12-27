f166d708199aeb43ace6ec7e1b8d80e8b908df72
SwockWickDus <70486244+SwockWickDus@users.noreply.github.com>
2020-12-26 21:01:43 -0600

    Add files via upload

---
+++ RockPaperScissors.py
@@ -0,0 +1,55 @@
+#initalizes the computer and imports the random integer
+from random import randint
+
+#make options into list and computer chooses based on integer indexing
+rpc = ["rock", "paper", "scissor"]
+comp = rpc[randint(0,2)]
+
+keepGoing = True
+
+while keepGoing != False:
+    choice = input("Choose either rock, paper, scissor or stop: ")
+
+    #Exit out of the game
+    if(choice == "stop"):
+        keepGoing = False
+
+    #both comp and user pick same choice
+    elif(choice == comp):
+        print("Lol it's a tie. the computer chose", comp)
+
+
+    #user inputs an option of rock and comp outcome
+    elif(choice == "rock"):
+        if(comp == "scissor"):
+            print("nice job, you won. comp chose", comp)
+        else:
+            print("damn son, you lost, computer chose", comp)
+            
+    #user inputs an option of scissor and comp outcome
+    elif(choice == "scissor"):
+        if(comp == "paper"):
+             print("nice job, you won. comp chose", comp)
+        else:
+            print("damn son, you lost, computer chose", comp)
+
+    #user inputs an option of paper and comp outcome
+    elif(choice == "paper"):
+        if(comp == "rock"):
+             print("nice job, you won. comp chose", comp)
+        else:
+            print("damn son, you lost, computer chose", comp)
+
+    #if user puts in something other than the 3 choices 
+    else:
+        print("Put in a valid option, you inputed something weird")
+
+    #this line of code is imporant so the computer doesn't
+    #give the same response and the person can repeat options.
+    comp = rpc[randint(0,2)]    
+
+    print("")
+
+
+    
+    
